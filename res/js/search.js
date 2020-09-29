06fea237f8d5f676a7e5038fbbce3b2adc4b62de
oghenefejiro ekebe <orobosafilomena@gmail.com>
2020-09-29 22:21:56 +0100

    added buttons

--- res/js/search.js
+++ res/js/search.js
@@ -4,3 +4,66 @@ $("#find").click(function(){
 $("#srch").val();
 });	
 });
+<div class="row horizontal mt-5 mx-auto">
+<div class="col vertical md-4 py-5">
+<div class="row-mt-5 mx-auto">
+<h5 class="text-center" id="Women-shoes">Women shoes</h5>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/women shoe combo.jpg"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/women shoe6.jpg"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div></div>
+<div class="row-mx-auto">
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/women shoe4.png"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/women shoe5.jpg"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div></div>
+<div class="row-mx-auto">
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/women shoe8.png"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/women shoe7.png"><figcaption class="text-center fig-caption"><s>&#36;1000</s> &#36;500</figcaption></figure>
+</div>
+</div>
+</div><div class="col-md-4 py-5">
+<div class="row-mx-auto">
+<h5 class="text-center" id="Men-shoes">Men shoes</h5>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/shoe4.png"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/shoe4.png"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div></div>
+<div class="row-mx-auto">
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/shoe.png"><figcaption class="text-center mb-5 fig-caption">Easy slide &#36;500</figcaption></figure>
+</div>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/shoe9.png"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div></div>
+<div class="row-mx-auto">
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/shoe5.png"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/shoe2.png"><figcaption class="text-center fig-caption" >Easy slide &#36;500</figcaption></figure>
+</div>
+</div></div>
+<div class="col-md-4">
+<div class="row-mx-auto">	
+<h5 class="text-center" id="Kids-shoes">Kids shoes</h5>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/baby shoe4.jpg"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/baby-shoes1.jpg"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div></div>
+<div class="row-mt-5 mx-auto">
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/baby-shoes2.jpg"><figcaption class="text-center mb-5 fig-caption">Easy slide &#36;500</figcaption></figure>
+</div>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/baby-shoes3.jpg"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure>
+</div></div>
+<<div class="row-mx-auto">
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/shoes col.jpg"><figcaption class="text-center fig-caption">Easy slide &#36;500</figcaption></figure></div>
+<div class="col-md-2 p-2"><figure><img class="img-fluid" src="res/img/shoes collection.jpg"><figcaption class="text-center fig-caption" >Easy slide &#36;500</figcaption></figure>
+</div>	
+</div></div></div>	
+</section>
+<section class="container">
+<div class="col-md-6 "><h5 id="Login">Login</h5>
+<input type="text" placeholder="Enter your password" 
+id="login" class="input mx-3 px-2">
+<button class="btn btn-secondary px-1">Enter</button>
+</div>
+</div>
+</section>
